daed488dcade58d946d75268ea930c719c961ed6
Alessandro D'Anna <cactusjack05@gmail.com>
2023-07-13 15:24:17 +0200

    playground creazione collegamento about

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,8 @@
         <v-btn block elevation="2">Bottone 2</v-btn>
       </v-col>
       <v-col cols="4">
-        <v-btn block elevation="2">Bottone 3</v-btn>
+        <!-- Router link serve per collegare le pagine -->
+        <router-link to="/about"><v-btn block elevation="2">About</v-btn></router-link>
       </v-col>
     </v-row>
   </v-container>
